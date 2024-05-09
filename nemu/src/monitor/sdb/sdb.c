9ac066c2b8a874bf435ccb21e783b3a33a4c1bab
tracer-ics2023 <tracer@njuics.org>
2024-05-09 10:30:47 +0800

    >  compile NEMU 221220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-105-generic #115-Ubuntu SMP Mon Apr 15 09:52:04 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  10:30:47 up  3:17,  1 user,  load average: 0.50, 0.39, 0.28

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -51,7 +51,10 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
   return -1;
 }
-
+static int cmd_si(char *args){
+  cpu_exec(1);
+  return 0;
+}
 static int cmd_help(char *args);
 
 static struct {
@@ -62,9 +65,8 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si [N]","program excutes n steps", cmd_si},
   /* TODO: Add more commands */
-
 };
 
 #define NR_CMD ARRLEN(cmd_table)
